09b4dccaa4522080b1683caecc58758c0d720a7e
uosfz <uosfz@student.kit.edu>
2025-02-02 01:07:31 +0100

    kruskal maze generation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+mod maze;
+
 use std::io;
 
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use crossterm::style::StyledContent;
+use maze::Maze;
 use ratatui::{
     buffer::Buffer,
     layout::Rect,
@@ -12,10 +15,17 @@ use ratatui::{
     DefaultTerminal, Frame,
 };
 
+#[derive(Debug, Copy, Clone)]
+pub enum Tile {
+    Free,
+    Wall,
+}
+
 #[derive(Debug, Default)]
 pub struct App {
     counter: u8,
     exit: bool,
+    field: Vec<Vec<Tile>>,
 }
 
 impl App {
@@ -41,14 +51,6 @@ impl App {
             );
         frame.render_widget(p, Rect::new(40, 10, 20, 20));
         */
-        let text = vec![
-            Line::from(vec![".".gray(), "@".green()]),
-            Line::from(vec![".".gray(), ".".gray()]),
-        ];
-        frame.render_widget(
-            Paragraph::new(text).block(Block::bordered()),
-            Rect::new(5, 5, 18, 18),
-        );
     }
 
     fn handle_events(&mut self) -> io::Result<()> {
@@ -107,12 +109,58 @@ impl Widget for &App {
             .centered()
             .block(block)
             .render(area, buf);
+
+        let text = vec![
+            Line::from(Vec::from_iter(self.field[0].iter().copied().map(
+                |tile| match tile {
+                    Tile::Free => ".".dark_gray(),
+                    Tile::Wall => "O".white(),
+                },
+            ))),
+            Line::from(Vec::from_iter(self.field[1].iter().copied().map(
+                |tile| match tile {
+                    Tile::Free => ".".dark_gray(),
+                    Tile::Wall => "O".white(),
+                },
+            ))),
+        ];
+        Paragraph::new(text)
+            .block(Block::bordered())
+            .render(Rect::new(4, 4, 18, 18), buf);
     }
 }
 
 fn main() -> io::Result<()> {
+    let maze = Maze::kruskal(16, 8);
+    println!("{}", maze);
+    return Ok(());
     let mut terminal = ratatui::init();
-    let app_result = App::default().run(&mut terminal);
+    let mut app = App {
+        counter: 0,
+        exit: false,
+        field: vec![
+            vec![
+                Tile::Free,
+                Tile::Free,
+                Tile::Wall,
+                Tile::Wall,
+                Tile::Free,
+                Tile::Free,
+                Tile::Free,
+                Tile::Wall,
+                Tile::Wall,
+                Tile::Free,
+                Tile::Free,
+                Tile::Free,
+                Tile::Wall,
+                Tile::Wall,
+                Tile::Free,
+                Tile::Wall,
+            ],
+            vec![Tile::Free; 16],
+        ],
+    };
+    let app_result = app.run(&mut terminal);
     ratatui::restore();
     app_result
 }
---
+++ src/maze.rs
@@ -0,0 +1,153 @@
+use std::fmt;
+
+use rand::seq::SliceRandom;
+
+#[derive(Debug)]
+struct UnionFind {
+    reps: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        Self {
+            reps: Vec::from_iter(0..n),
+        }
+    }
+
+    fn rep(&self, mut a: usize) -> usize {
+        while self.reps[a] != a {
+            a = self.reps[a];
+        }
+        a
+    }
+
+    fn in_same_set(&self, a: usize, b: usize) -> bool {
+        self.rep(a) == self.rep(b)
+    }
+
+    fn join(&mut self, a: usize, b: usize) {
+        let rep_b = self.rep(b);
+        self.reps[rep_b] = a;
+    }
+}
+
+impl fmt::Display for UnionFind {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "UnionFind {{ ")?;
+        for rep in 0..self.reps.len() {
+            if self.reps[rep] != rep {
+                continue;
+            }
+            // find everything belonging to this rep
+            write!(f, "[")?;
+            let mut first = true;
+            for member in 0..self.reps.len() {
+                if self.in_same_set(member, rep) {
+                    if first {
+                        first = false;
+                        write!(f, "{}({})", member, self.reps[member])?;
+                    } else {
+                        write!(f, ", {}({})", member, self.reps[member])?;
+                    }
+                }
+            }
+            write!(f, "] ")?;
+        }
+        write!(f, "}}")?;
+        Ok(())
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum Tile {
+    Free,
+    Wall,
+}
+
+pub struct Maze {
+    tiles: Vec<Vec<Tile>>,
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Pos(usize, usize);
+
+fn is_horizontal_edge(pos: Pos) -> bool {
+    pos.0 % 2 == 0
+}
+
+fn node_to_idx(pos: Pos, nx: usize, ny: usize) -> usize {
+    (pos.1 / 2) * nx + (pos.0) / 2
+}
+
+impl Maze {
+    pub fn empty(nx: usize, ny: usize) -> Self {
+        let mut maze = Maze { tiles: Vec::new() };
+        for line in 0..(2 * ny + 1) {
+            if line % 2 == 0 {
+                maze.tiles.push(vec![Tile::Wall; 2 * nx + 1]);
+            } else {
+                let mut ln = Vec::new();
+                for _ in 0..nx {
+                    ln.push(Tile::Wall);
+                    ln.push(Tile::Free);
+                }
+                ln.push(Tile::Wall);
+                maze.tiles.push(ln);
+            }
+        }
+        maze
+    }
+
+    pub fn kruskal(nx: usize, ny: usize) -> Self {
+        let mut maze = Self::empty(nx, ny);
+        let mut edges = Vec::new();
+        // horizontal
+        for y in 0..ny {
+            for x in 1..nx {
+                edges.push(Pos(2 * x, 2 * y + 1));
+            }
+        }
+        // vertical
+        for x in 0..nx {
+            for y in 1..ny {
+                edges.push(Pos(2 * x + 1, 2 * y));
+            }
+        }
+        edges.shuffle(&mut rand::rng());
+        println!("{:?}", edges);
+
+        let mut sets = UnionFind::new(nx * ny);
+
+        for edge in edges {
+            let (pos_a, pos_b) = if is_horizontal_edge(edge) {
+                (Pos(edge.0 - 1, edge.1), Pos(edge.0 + 1, edge.1))
+            } else {
+                (Pos(edge.0, edge.1 - 1), Pos(edge.0, edge.1 + 1))
+            };
+            let (index_a, index_b) = (node_to_idx(pos_a, nx, ny), node_to_idx(pos_b, nx, ny));
+            if !sets.in_same_set(index_a, index_b) {
+                sets.join(index_a, index_b);
+                maze.tiles[edge.1][edge.0] = Tile::Free;
+                println!("{}", maze);
+                println!("{}", sets);
+            }
+        }
+
+        maze
+    }
+}
+
+impl fmt::Display for Maze {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for line in &self.tiles {
+            for tile in line {
+                match tile {
+                    Tile::Free => write!(f, " ")?,
+                    Tile::Wall => write!(f, "O")?,
+                }
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
